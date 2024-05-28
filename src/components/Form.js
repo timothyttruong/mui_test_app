50a1add0c4a0f6363b479f006bd1f854429a44a2
Timothy Truong <timothy.tt.truong@gmail.com>
2024-05-28 16:42:18 -0500

    Third attempt

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import { Box, Typography, Checkbox, Radio, Button, TextField } from '@mui/materi
 import Header from './Header';
 
 const Form = () => {
-    const [name, setName] = useState(""); 
+    const [name, setName] = useState("");
 
     const [email, setEmail] = useState("");
 
@@ -18,19 +18,19 @@ const Form = () => {
 
         const errorObj = {};
 
-        if (!name){ //if name is emptu
+        if (!name) { //if name is emptu
             errorObj.name = "Name is required!";
         }
 
-        if (!email){
+        if (!email) {
             errorObj.email = "Email is required!";
         }
         else if (!/\S+@\S+\.\S+/.test(email)) { //function to check if email is of the correct form
             errorObj.email = "Invalid email format!";
         }
 
-        if (Object.keys(errorObj).length === 0){   //Objects.key returns keys of an array, .length returns the length of that array => If there is no error or error object is empty
-            console.log("Form submitted", {name, email, termsAccepted, gender});
+        if (Object.keys(errorObj).length === 0) {   //Objects.key returns keys of an array, .length returns the length of that array => If there is no error or error object is empty
+            console.log("Form submitted", { name, email, termsAccepted, gender });
             console.log("Clean the states")
             setName("");
             setEmail("");
@@ -41,68 +41,68 @@ const Form = () => {
             setError(errorObj);
         }
 
-        
+
     }
 
-  return (<>
-    <Box>
-     <Header />
-    </Box>
-    <Box sx={{display:"flex", justifyContent:"center"}}>
-        
+    return (<>
+        <Box>
+            <Header />
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
 
-        <form onSubmit={handleSubmission} style={{width: "20%"}}>
-            <Typography variant='h5'>
-                Simple Form
-            </Typography>
 
-            <Box margin={4} marginLeft={0}>
-                <TextField fullWidth id="name" 
-                value ={name}
-                label="Name"
-                onChange={(e) => setName(e.target.value)} 
-                error={!!error.name}
-                helperText={error.name}/>
-            </Box>
-            
-            <Box margin={4} marginLeft={0}>
-                <TextField fullWidth
-                label="Email" value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                error={!!error.email}
-                helperText={error.email}
-                placeholder='Email'/>
-            </Box>
-            
+            <form onSubmit={handleSubmission} style={{ width: "20%" }}>
+                <Typography variant='h5'>
+                    Simple Form
+                </Typography>
 
-            <Box>
-                <Checkbox check={termsAccepted} onChange={(e) => {setTermAccepted(e.target.checked)}} />
+                <Box margin={4} marginLeft={0}>
+                    <TextField fullWidth id="name"
+                        value={name}
+                        label="Name"
+                        onChange={(e) => setName(e.target.value)}
+                        error={!!error.name}
+                        helperText={error.name} />
+                </Box>
 
-                <label htmlFor='termsAccepted'>I accept the terms and conditions</label>
-            </Box>
+                <Box margin={4} marginLeft={0}>
+                    <TextField fullWidth
+                        label="Email" value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        error={!!error.email}
+                        helperText={error.email}
+                        placeholder='Email' />
+                </Box>
 
-            <Box>
-                <Typography variant='subtitle1'>Gender:</Typography>
 
-                <Radio checked={gender == "male"} 
-                onChange={() => setGender("male")}
-                value="male" />  
-                <label htmlFor='male'>Male</label>
+                <Box>
+                    <Checkbox check={termsAccepted} onChange={(e) => { setTermAccepted(e.target.checked) }} />
 
-                <Radio checked={gender == "female"} 
-                onChange={() => setGender("female")}
-                value="female" />  
-                <label htmlFor='female'>Female</label>
-            </Box>
+                    <label htmlFor='termsAccepted'>I accept the terms and conditions</label>
+                </Box>
 
-            <Box sx={{display:"flex", justifyContent:"center"}}>
-                <Button variant="contained" type="submit" color="primary">SUBMIT</Button>
-            </Box>
-            
-        </form>
-    </Box>
+                <Box>
+                    <Typography variant='subtitle1'>Gender:</Typography>
+
+                    <Radio checked={gender == "male"}
+                        onChange={() => setGender("male")}
+                        value="male" />
+                    <label htmlFor='male'>Male</label>
+
+                    <Radio checked={gender == "female"}
+                        onChange={() => setGender("female")}
+                        value="female" />
+                    <label htmlFor='female'>Female</label>
+                </Box>
+
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Button variant="contained" type="submit" color="primary">SUBMIT</Button>
+                </Box>
+
+            </form>
+        </Box>
     </>
-  )
+    )
 }
 
 export default Form
